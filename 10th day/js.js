296e928d1ec4388cb44c9895fae1a950975c2dac
Ritik Kumar <ritikkumar12bicbly@gmail.com>
2025-08-07 12:09:40 +0530

    moving all the zeroes to the end

--- 10th day/js.js
+++ 10th day/js.js
@@ -662,5 +662,24 @@
 // }
 // console.log(twoSum([1,2,4,5,6,8],10));
 
-// move all zeros to end
-// [1,2,0,6,0,6,0]==>output==>[1,2,6,6,0,0,0]
+// move all the zeroes to the end 
+// input : [1,32,0,0,42,44,2,5,2,0]
+// function moveZeroesToEnd(arr) {
+//     let result = [];
+//     let zeroCount = 0;
+//     for (let num of arr) {
+//         if (num == 0) {
+//             zeroCount++;
+//         } else {
+//             result.push(num);
+//         }
+//     }
+//     while (zeroCount) {
+//         result.push(0);
+//         zeroCount--;
+//     }
+//     return result;
+// }
+
+// const input = [1,32,0,0,42,44,2,5,2,0];
+// console.log(moveZeroesToEnd(input));
