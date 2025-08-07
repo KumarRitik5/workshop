821df0dfd39064864ca379c496b98e391f1a5166
Ritik Kumar <ritikkumar12bicbly@gmail.com>
2025-08-07 13:46:06 +0530

    // find the max sum from the two numbers of the ar ray

--- 10th day/js.js
+++ 10th day/js.js
@@ -683,3 +683,33 @@
 
 // const input = [1,32,0,0,42,44,2,5,2,0];
 // console.log(moveZeroesToEnd(input));
+
+// [2,-4,-4,6,-5,0,-3,5,9]
+// find the max sum from the two numbers of the array 
+// [2,-4,-4,,6,-5,0,-3,5,9]
+// if the sum is the negative make it the zero 
+
+let numbers = [2,-4,-4,,6,-5,0,-3,5,9]
+
+
+function findMaxSum(numbers){
+    let maxSum = Number.NEGATIVE_INFINITY;
+    for (let i = 0; i < numbers.length; i++) {
+    for (let j = i + 1; j < numbers.length; j++) {
+        if (numbers[i] !== undefined && numbers[j] !== undefined) {
+            let sum = numbers[i] + numbers[j];
+            if (sum > maxSum) {
+                maxSum = sum;
+            }
+        }
+    }
+}
+    if (maxSum < 0) {
+    maxSum = 0;
+}
+    return maxSum
+}
+
+
+
+console.log(findMaxSum(numbers));
