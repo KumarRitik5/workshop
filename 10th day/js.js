7394b5efbb8d61e4671bafce06ac431f7a8765f6
Ritik Kumar <ritikkumar12bicbly@gmail.com>
2025-08-07 13:47:17 +0530

    saedfgtyhjk

--- 10th day/js.js
+++ 10th day/js.js
@@ -689,27 +689,27 @@
 // [2,-4,-4,,6,-5,0,-3,5,9]
 // if the sum is the negative make it the zero 
 
-let numbers = [2,-4,-4,,6,-5,0,-3,5,9]
+// let numbers = [2,-4,-4,,6,-5,0,-3,5,9]
 
 
-function findMaxSum(numbers){
-    let maxSum = Number.NEGATIVE_INFINITY;
-    for (let i = 0; i < numbers.length; i++) {
-    for (let j = i + 1; j < numbers.length; j++) {
-        if (numbers[i] !== undefined && numbers[j] !== undefined) {
-            let sum = numbers[i] + numbers[j];
-            if (sum > maxSum) {
-                maxSum = sum;
-            }
-        }
-    }
-}
-    if (maxSum < 0) {
-    maxSum = 0;
-}
-    return maxSum
-}
+// function findMaxSum(numbers){
+//     let maxSum = Number.NEGATIVE_INFINITY;
+//     for (let i = 0; i < numbers.length; i++) {
+//     for (let j = i + 1; j < numbers.length; j++) {
+//         if (numbers[i] !== undefined && numbers[j] !== undefined) {
+//             let sum = numbers[i] + numbers[j];
+//             if (sum > maxSum) {
+//                 maxSum = sum;
+//             }
+//         }
+//     }
+// }
+//     if (maxSum < 0) {
+//     maxSum = 0;
+// }
+//     return maxSum
+// }
 
 
 
-console.log(findMaxSum(numbers));
+// console.log(findMaxSum(numbers));
