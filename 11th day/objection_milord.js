2788712da884bc23469ab10e8abd15551d099359
Ritik Kumar <ritikkumar12bicbly@gmail.com>
2025-08-08 11:17:13 +0530

    Comment out frequency calculation and first non-repeating element logic for future implementation

--- 11th day/objection_milord.js
+++ 11th day/objection_milord.js
@@ -28,31 +28,31 @@
 // console.log(Object.keys(details));
 // console.log(Object.values(details));
 
-numbers = [1,3,4,3,5,1,3,5,2,23,4,1,2];
-// find frequency of each element from array
-let data = {};
-for (let num of numbers) {
-    data[num] = (data[num] || 0) + 1;
-}
-console.log(data);
+// numbers = [1,3,4,3,5,1,3,5,2,23,4,1,2];
+// // find frequency of each element from array
+// let data = {};
+// for (let num of numbers) {
+//     data[num] = (data[num] || 0) + 1;
+// }
+// console.log(data);
 
-// find first non repeating element from array
-for (let key in data) {
-    if (data[key] == 1) {
-        console.log("First non-repeating element is:", key);
-        break;
-    }
-}
+// // find first non repeating element from array
+// for (let key in data) {
+//     if (data[key] == 1) {
+//         console.log("First non-repeating element is:", key);
+//         break;
+//     }
+// }
 
-// invert the object (reverse key value)
-let reverse={}
-for (let key in data){
-    reverse[data[key]]=key
-}
-console.log(reverse);
-// merge two objects
-let data1={1:5,2:10,3:15}
-let data2={1:7,4:10,3:10}
+// // invert the object (reverse key value)
+// let reverse={}
+// for (let key in data){
+//     reverse[data[key]]=key
+// }
+// console.log(reverse);
+// // merge two objects
+// let data1={1:5,2:10,3:15}
+// let data2={1:7,4:10,3:10}
 
 // let result={};
 // for(let key in data1){
