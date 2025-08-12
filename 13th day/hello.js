fb2df355dcdd64bf35f78db7c899908905e432b2
Ritik Kumar <ritikkumar12bicbly@gmail.com>
2025-08-12 09:59:39 +0530

    Add destructuring example with object properties in hello.js

---
+++ 13th day/hello.js
@@ -0,0 +1,8 @@
+// destructuring
+const arr ={"one":"hello","two":"two","three":"three","four":"four"}
+// const one=arr[0]
+const {one, two, three, four} = arr
+console.log(one);
+console.log(two);
+console.log(three);
+console.log(four);
