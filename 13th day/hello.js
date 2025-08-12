e53cfdec54ce8c60ccade09aae5c641ab678a92c
Ritik Kumar <ritikkumar12bicbly@gmail.com>
2025-08-12 10:07:27 +0530

    Refactor hello.js to comment out unused destructuring example and focus on array destructuring

--- 13th day/hello.js
+++ 13th day/hello.js
@@ -1,8 +1,14 @@
 // destructuring
-const arr ={"one":"hello","two":"two","three":"three","four":"four"}
+// const arr ={"one":"hello","two":"two","three":"three","four":"four"}
 // const one=arr[0]
-const {one, two, three, four} = arr
-console.log(one);
-console.log(two);
-console.log(three);
-console.log(four);
+// const {one, two, three, four} = arr
+// console.log(one);
+// console.log(two);
+// console.log(three);
+// console.log(four);
+
+const hello = [1,2,5]
+const [d,f] = hello
+
+console.log(d);
+console.log(f);
